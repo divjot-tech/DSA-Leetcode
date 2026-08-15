64d5b91a72f0d0ec65f1fbe1792d664b5f648d16
Divjot singh <168454365+divjot-tech@users.noreply.github.com>
2026-08-15 16:53:07 +0530

    Time: 4 ms (18.88%), Space: 95.2 MB (42.6%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,54 +1,47 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
-        // Always binary search on the smaller array
-        if (nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2, nums1);
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+        int n1= a.size();
+        int n2 = b.size();
+        int n = n1+n2;
+        int cnt =0;
+        int i = 0;
+        int j=0;
+        int ind2=n/2;
+        int ind1 = ind2-1;
+        int ind1el = -1;
+        int ind2el = -1;
+
+        while(i<n1 && j<n2){
+            if(a[i]<b[j]){
+                if(cnt==ind1) ind1el=a[i];
+                if(cnt==ind2) ind2el=a[i];
+                i++;
+            }
+            else{
+                if(cnt==ind1) ind1el=b[j];
+                if(cnt==ind2) ind2el=b[j];
+                j++;
+
+            }
+            cnt++;
         }
-
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-
-        int low = 0;
-        int high = n1;
-
-        while (low <= high) {
-
-            int cut1 = (low + high) / 2;
-            int cut2 = (n1 + n2 + 1) / 2 - cut1;
-
-            int left1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
-            int left2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
-
-            int right1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
-            int right2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
-
-            // Correct partition
-            if (left1 <= right2 && left2 <= right1) {
-
-                // Odd total length
-                if ((n1 + n2) % 2 == 1) {
-                    return max(left1, left2);
-                }
-
-                // Even total length
-                return (max(left1, left2) + min(right1, right2)) / 2.0;
+            while(i<n1){
+                if(cnt==ind1) ind1el=a[i];
+                if(cnt==ind2) ind2el=a[i];
+                cnt++;
+                i++;
             }
-
-            // We need to move cut1 left
-            else if (left1 > right2) {
-                high = cut1 - 1;
+            while(j<n2){
+                if(cnt==ind1) ind1el=b[j];
+                if(cnt==ind2) ind2el=b[j];
+                cnt++;
+                j++;
             }
+        if(n%2==1) return ind2el;
+        else return (double)((double)(ind1el+ind2el)/2.0);
 
-            // We need to move cut1 right
-            else {
-                low = cut1 + 1;
-            }
-        }
 
-        return 0.0;
-    
         
     }
 };
